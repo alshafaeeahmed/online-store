b828553b596d4159f8dd68b843d9e77717b78352
Ahmed Alshafaee <58431974+alshafaeeahmed@users.noreply.github.com>
2023-08-10 23:30:24 +0300

    making project structurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
