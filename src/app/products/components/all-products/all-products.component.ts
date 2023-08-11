d9202f56ac6233562ea39bad968af406519b8443
Ahmed Alshafaee <58431974+alshafaeeahmed@users.noreply.github.com>
2023-08-11 11:21:07 +0300

    Filter By Category

--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -9,19 +9,42 @@ import { Router } from '@angular/router';
 })
 export class AllProductsComponent implements OnInit {
   products: any[] = [];
+  categories: any[] = [];
   constructor(private servidce: ProductsService, private router: Router) {}
   ngOnInit(): void {
     this.getProducts();
+    this.getCategories();
   }
   getProducts() {
     this.servidce.getAllProducts().subscribe(
       (res: any) => {
         this.products = res;
+      },
+      (error) => {
+        this.router.navigate(['/error']);
+      }
+    );
+  }
+  getCategories() {
+    this.servidce.getAllCategories().subscribe(
+      (res: any) => {
+        this.categories = res;
         console.log(res);
       },
       (error) => {
         this.router.navigate(['/error']);
       }
     );
+  }
+  filterCategorie(event: any) {
+    let value = event.target.value;
+    value == 'all' ? this.getProducts(): this.getProductCategorie(value);
+  }
+  getProductCategorie(categorieType: string) {
+    this.servidce
+      .getProductByCategories(categorieType)
+      .subscribe((res: any) => {
+        this.products = res;
+      });
   }
 }
