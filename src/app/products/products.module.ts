15ab900fd238547d78fc2bd82f88b21eade1ab1c
Ahmed Alshafaee <58431974+alshafaeeahmed@users.noreply.github.com>
2023-08-11 12:03:07 +0300

    Make Spinner with Requests

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,16 +2,19 @@
 import { CommonModule } from '@angular/common';
 import { AllProductsComponent } from './components/all-products/all-products.component';
 import { ProductsDetailsComponent } from './components/products-details/products-details.component';
+import { SpinnerComponent } from '../shared/components/spinner/spinner.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     AllProductsComponent,
-    ProductsDetailsComponent
+    ProductsDetailsComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
   ]
 })
 export class ProductsModule { }
